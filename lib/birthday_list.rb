cb21d9f3692d18811abfaafc229b22904def8baa
Jessica Marais <jessica_marais@hotmail.com>
2019-09-18 23:29:09 +0100

    Need to initialize Date

--- lib/birthday_list.rb
+++ lib/birthday_list.rb
@@ -1,10 +1,43 @@
 class BirthdayList
+  DATE_FORMAT = '%m-%d'
 # Initialize instance variable @list and declare as an empty array, as we'll push name and birthday as a hash into it
   def initialize
     @list = []
   end
-# Pass name and birthday as an argument and push each name & birthday into a hash, into the list array. 
+# Pass name and birthday as an argument and push each name & birthday into a hash, into the list array.
   def add(name, birthday)
     @list << {name: name, birthday: birthday}
   end
+
+  def show
+    @list.each do |bday|
+      puts "#{bday[:name]}: #{bday[:birthday]}"
+    end
+  end
+
+  def check
+    @list.each do |bday|
+# Need to create today?(bday) method
+      puts "Today is #{[:name]}'s birthday." if today?(bday)
+    end
+  end
+
+# Method to see if their birthday matches with today's date.
+  def today?(bday)
+    birthday_date(bday) == today
+  end
+
+# Define the birthday date into a readable date format, to be called in the today? method.
+  def birthday_date(bday)
+# Date.parse creates date object
+# .strftime formats dates according to format given in DATE_FORMAT (see top)
+    Date.parse(bday[:birthday]).strftime(DATE_FORMAT)
+    # *****NEED TO REQUIRE DATE FROM ANOTHER FILE**** 
+  end
+
+# Define today's date in a method - need to know today's date for today? method.
+  def today
+    Time.now.strftime(DATE_FORMAT)
+  end
+
 end
--- spec/birthday_list_spec.rb
+++ spec/birthday_list_spec.rb
@@ -13,4 +13,16 @@ describe BirthdayList do
     subject.add("Jess", "9th May 1994")
     expect(subject.add("James", "13th Sept 1991")).to eq [{name: "Jess", birthday: "9th May 1994"}, {name: "James", birthday: "13th Sept 1991"}]
   end
+
+  it "show all names and birthdays from list" do
+    subject.add("Jess", "9th May 1994")
+    subject.add("James", "13th Sept 1991")
+    expect{subject.show}.to output("Jess: 9th May 1994\nJames: 13th Sept 1991\n").to_stdout
+  end
+
+  it "checks if today is someone's birthday" do
+    subject.add("Jess", "9th May 1994")
+    subject.add("James", "18th Sept 2019")
+    expect{subject.check}.to output("Today is James' birthday.\n").to_stdout
+  end
 end
