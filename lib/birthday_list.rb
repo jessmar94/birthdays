6212c40d605f534825922250e4ba0d208b4210fa
Jessica Marais <jessica_marais@hotmail.com>
2019-09-18 22:54:33 +0100

    Adds two birthdays test

--- lib/birthday_list.rb
+++ lib/birthday_list.rb
@@ -1,9 +1,9 @@
 class BirthdayList
-#
+# Initialize instance variable @list and declare as an empty array, as we'll push name and birthday as a hash into it
   def initialize
     @list = []
   end
-
+# Pass name and birthday as an argument and push each name & birthday into a hash, into the list array. 
   def add(name, birthday)
     @list << {name: name, birthday: birthday}
   end
--- spec/birthday_list_spec.rb
+++ spec/birthday_list_spec.rb
@@ -7,4 +7,10 @@ describe BirthdayList do
   it "adds a birthday" do
     expect(subject.add("Jess", "9th May 1994")).to eq [{name: "Jess", birthday: "9th May 1994"}]
   end
+
+# Add two birthdays to list
+  it "adds two birthdays" do
+    subject.add("Jess", "9th May 1994")
+    expect(subject.add("James", "13th Sept 1991")).to eq [{name: "Jess", birthday: "9th May 1994"}, {name: "James", birthday: "13th Sept 1991"}]
+  end
 end
